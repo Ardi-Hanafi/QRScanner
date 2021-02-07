b69b29fea203c094f3c5f883ba90b38e24a6d4b0
Ardi-Hanafi <Ardi-Hanafi@users.noreply.github.com>
2021-02-07 16:17:52 +0700

    Update Search View

--- App.js
+++ App.js
@@ -6,7 +6,9 @@ import {
   StyleSheet,
   View,
   Text,
+  TextInput,
   StatusBar,
+  TouchableOpacity,
 } from 'react-native';
 
 
@@ -22,10 +24,18 @@ const App = () => {
         <View style={styles.cameraCon}>
           <QRCodeScanner
             onRead={readQR}
-
           />
         </View>
-      
+        <View style={styles.searchCon}>
+          <View style={styles.garis}/>
+          <Text style={styles.text}>Atau input ID Siswa Manual</Text>
+          <TextInput
+            style={styles.input}
+          />
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnText}>Cari Siswa</Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     </>
   );
@@ -33,13 +43,58 @@ const App = () => {
 
 const styles = StyleSheet.create({
   cameraCon: {
-    height: 'auto', width: '100%',
-    backgroundColor: 'red'
+    height: '70%', width: '100%',
+    backgroundColor: '#ffffff'
+  },
+  searchCon: {
+    display:'flex',
+    // justifyContent:'center',
+    alignItems:'center',
+    position:'absolute',
+    bottom: 0,
+    height: '30%', width: '100%',
+    backgroundColor: '#ffffff',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
   },
   container: {
     display:'flex',
     height: '100%', width: '100%',
-    backgroundColor: 'blue'
+  },
+  garis: {
+    height:5, width:'20%',
+    backgroundColor: '#D1D5DB',
+    marginTop: 7,
+    borderRadius: 5,
+  },
+  text: {
+    fontSize:16,
+    color: '#52525B',
+    marginTop: 20,
+  },
+  input: {
+    width:'80%', height:50,
+    backgroundColor: '#FAFAFA',
+    marginTop: 15,
+    borderRadius: 8,
+    borderWidth: 1, borderColor:'#A1A1AA',
+    fontSize: 16,
+    color: '#4d4d4d',
+    textAlign:'center'
+  },
+  btn: {
+    display:'flex',
+    justifyContent: 'center',
+    alignItems:'center',
+    width: '80%', height: 50,
+    marginTop: 15,
+    backgroundColor: '#10B981',
+    borderRadius: 8
+  },
+  btnText: {
+    fontSize: 16,
+    color: '#ffffff',
+    fontWeight:'bold',
   }
 });
 
